d4270095081686f24ca5dbf6c00d1689b4cb02f0
TheMostOriginalNamePossible <44122656+TheMostOriginalNamePossible@users.noreply.github.com>
2024-06-10 19:20:37 -0400

    just added some stuff

---
+++ gameSprites.py
@@ -0,0 +1,143 @@
+"""sprites.py
+Holds sprite classes. Put sprite classes in here.
+(For szhoe) Make sure you put a little comment on stuff you change or add
+"""
+
+import pygame, simpleGE
+
+
+class Sprite(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("INLYSSunny.png")
+
+        # hitbox coefficient private attributes
+
+        self.__topHitboxC = 1
+        self.__bottomHitboxC = 1
+        self.__leftHitboxC = 1
+        self.__rightHitboxC = 1
+
+        # hitbox coefficient properties
+
+        self.tophitboxC = 1
+        self.bottomHitboxC = 1
+        self.leftHitboxC = 1
+        self.rightHitboxC = 1
+
+        self.setSize(100, 100)
+
+        # sets hitbox
+
+        self.setHitbox()
+
+
+    @property
+    def topHitboxC(self):
+        return self.__topHitboxC
+
+    @topHitboxC.setter
+    def topHitboxC(self, c):
+        if type(c) == float and 0 <= c:
+            self.__topHitboxC = c
+        else:
+            self.__topHitboxC = 1
+
+    @property
+    def bottomHitboxC(self):
+        return self.__bottomHitboxC
+
+    @bottomHitboxC.setter
+    def bottomHitboxC(self, c):
+        if type(c) == float and 0 <= c:
+            self.__bottomHitboxC = c
+        else:
+            self.__bottomHitboxC = 1
+
+    @property
+    def leftHitboxC(self):
+        return self.__leftHitboxC
+
+    @leftHitboxC.setter
+    def leftHitboxC(self, c):
+        if type(c) == float and 0 <= c:
+            self.__leftHitboxC = c
+        else:
+            self.__leftHitboxC = 1
+
+    @property
+    def rightHitboxC(self):
+        return self.__rightHitboxC
+
+    @rightHitboxC.setter
+    def rightHitboxC(self, c):
+        if type(c) == float and 0 <= c:
+            self.__rightHitboxC = c
+        else:
+            self.__rightHitboxC = 1
+
+    def getMousePos(self):
+        return pygame.mouse.get_pos()
+
+    def moveSprite(self):
+        pos = self.getMousePos()
+        self.x = pos[0]
+        self.y = pos[1]
+
+    def setHitbox(self):
+        self.top = self.top*self.topHitboxC
+        self.bottom = self.bottom*self.bottomHitboxC
+        self.left = self.left*self.leftHitboxC
+        self.right = self.right*self.rightHitboxC
+
+
+
+
+
+class Player(Sprite)
+    def __init__(self, scene):
+        super().__init__(scene)
+
+
+
+
+
+class FallingObject(Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.minSpeed = 3
+        self.maxSpeed = 8
+        self.value = 1
+        self.speed = 3
+
+    def reset(self):
+        # move to where drop comes f
+        self.y = 10
+
+        # y is random number between min and max speed
+        self.dy = self.speed
+
+    def checkBounds(self):
+        if self.top > self.screenHeight:
+            return True
+        else:
+            return False
+
+class PowerUp(FallingObject):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.yDropLocation = 10
+        self.xDropLocation = 10
+
+    def reset(self):
+        # tells where it drops from
+
+        self.x = self.xDropLocation
+        self.y = self.yDropLocation
+
+        # tells the speed at which it falls
+        self.dy = self.speed
+
+
+
+
--- joshuaInvadersMain.py
+++ joshuaInvadersMain.py
@@ -3,22 +3,46 @@ This will be used exclusively to run the game.
 
 """
 
-import simpleGE, sprites, pygame
+import simpleGE, pygame, json, gameSprites
+#Custom Events
 PAUSED = pygame.event.custom_type()
+
 class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__(self)
+        pygame.mixer.music.load("music file")
         self.setImage("image file")
         self.waveNumber = 1
-        self.player = self.sprites.Player
+
+        # put sprites here
+        self.player = gameSprites.Player(self)
+
+        self.tempInstances = []
         self.sprites = []
+        # make sounds here
+
+    def removeInstance(self, instance):
+        for sprite in self.sprites:
+            if sprite == instance:
+                self.sprites.remove(instance)
+
+    def createInstance(self, instance):
+        self.sprites.append(instance)
 
     def pauseGame(self):
-        # pauses the game (placeholder)
+        # temporary just so I remember
+        pygame.event.post(pygame.event.Event(PAUSED))
         pass
+
+    def quitGame(self):
+        # temporary just so I remember
+        pygame.event.post(pygame.event.Event(pygame.QUIT))
+        pass
+
     def saveGame(self):
         # placeholder to figure out how save games work
         pass
+
     def doEvents(self, event):
         # probably for game transitions like waves and stuff
         if event.type == PAUSED:
@@ -31,9 +55,17 @@ class Game(simpleGE.Scene):
         for event in pygame.event.get():
             self.doEvents(event)
 
+    def update(self):
+        isPlaying = pygame.mixer.music.get_busy()
+        if not isPlaying:
+            pygame.mixer.music.play()
+
 
 
 def main():
+    events = pygame.event.get()
+    print(PAUSED)
 
-if __name__ == "__main__"
+
+if __name__ == "__main__":
     main()
