92e24204473e2800873fab8f60b4cd3429f4ae92
TheMostOriginalNamePossible <44122656+TheMostOriginalNamePossible@users.noreply.github.com>
2024-06-13 02:02:17 -0400

    just added some stuff

--- gameSprites.py
+++ gameSprites.py
@@ -1,38 +1,43 @@
-"""sprites.py
-Holds sprite classes. Put sprite classes in here.
-(For szhoe) Make sure you put a little comment on stuff you change or add
+"""gameSprites.py
+Holds game assets. A bit of a misnomer.
 """
 
-import pygame, simpleGE, math
+import pygame, simpleGE, math, random
+
+
+class Sound(simpleGE.Sound):
+    def __init__(self, file):
+        self.sound = pygame.mixer.Sound(file)
+
+    def volume(self, value):
+        if type(value) == float:
+            self.sound.set_volume(value)
+        else:
+            self.sound.set_volume(0.5)
+
+
+class LblWave(simpleGE.MultiLabel):
+    pass
+
+class LblLives(simpleGE.Label):
+
+    pass
 
 class Path(object):
-    def __init__(self, scene):
-        super().__init__(scene)
-        self.points = ((0.0, 0.0), (1.0, 1.0))
+    def __init__(self):
+        super().__init__()
+        self.points = ((0, 0), (1, 1))
         #path parameter
-        self.__parameter = 0.0
-
-        self.parameter = 0.0
-
-    @property
-    def parameter(self):
-        return self.__parameter
-
-    @parameter.setter
-    def parameter(self, parameter):
-        if type(parameter) == float and 0.0 <= parameter <= 1.0:
-            self.parameter = parameter
-        else:
-            self.parameter = 0.0
+        self.t = 0.0
 
     def bezier(self, t, points):
         n = len(points)
         xSum = points[0][0]*math.pow(1-t, n)
         ySum = points[0][1]*math.pow(1-t, n)
         for i in range(1, n):
-            x_i = points[i][0]*math.pow(1-t, n-i)*math.pow(t, i)
+            x_i = points[i][0]*math.pow(1-t, n-i)*math.pow(t, i)*math.comb(n, i)
             xSum += x_i
-            y_i = points[i][1]*math.pow(1-t, n-i)*math.pow(t, i)
+            y_i = points[i][1]*math.pow(1-t, n-i)*math.pow(t, i)*math.comb(n, i)
             ySum += y_i
         p = (xSum, ySum)
         return p
@@ -41,75 +46,6 @@ class Path(object):
 class Sprite(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
-
-        # hitbox coefficient private attributes
-
-        self.__topHitboxC = 1
-        self.__bottomHitboxC = 1
-        self.__leftHitboxC = 1
-        self.__rightHitboxC = 1
-
-        # hitbox coefficient properties
-
-        self.topHitboxC = 1
-        self.bottomHitboxC = 1
-        self.leftHitboxC = 1
-        self.rightHitboxC = 1
-
-        # sets hitbox
-
-        self.setHitbox()
-
-
-    @property
-    def topHitboxC(self):
-        return self.__topHitboxC
-
-    @topHitboxC.setter
-    def topHitboxC(self, c):
-        if type(c) == float and 0 <= c:
-            self.__topHitboxC = c
-        else:
-            self.__topHitboxC = 1
-
-    @property
-    def bottomHitboxC(self):
-        return self.__bottomHitboxC
-
-    @bottomHitboxC.setter
-    def bottomHitboxC(self, c):
-        if type(c) == float and 0 <= c:
-            self.__bottomHitboxC = c
-        else:
-            self.__bottomHitboxC = 1
-
-    @property
-    def leftHitboxC(self):
-        return self.__leftHitboxC
-
-    @leftHitboxC.setter
-    def leftHitboxC(self, c):
-        if type(c) == float and 0 <= c:
-            self.__leftHitboxC = c
-        else:
-            self.__leftHitboxC = 1
-
-    @property
-    def rightHitboxC(self):
-        return self.__rightHitboxC
-
-    @rightHitboxC.setter
-    def rightHitboxC(self, c):
-        if type(c) == float and 0 <= c:
-            self.__rightHitboxC = c
-        else:
-            self.__rightHitboxC = 1
-
-    def setHitbox(self):
-        self.top = self.top*self.topHitboxC
-        self.bottom = self.bottom*self.bottomHitboxC
-        self.left = self.left*self.leftHitboxC
-        self.right = self.right*self.rightHitboxC
 
     def checkBounds(self):
         if self.bottom > self.screenHeight:
@@ -128,43 +64,62 @@ class Joshua(Sprite):
     def __init__(self, scene):
         super().__init__(scene)
         self.setImage("Joshua.png")
-        self.setSize(80, 80)
+        self.setSize(60, 60)
         self.position = (50, 50)
-        self.hp = 5
+        self.damage = 1
+        self.hp = 3
+        self.path = Path()
+        #
+        self.dropChance = 0.1
+        self.perTickShootChance = 0.01
 
-        self.setHitbox()
+        self.timer = simpleGE.Timer()
+        self.timer.totalTime = 5
+        self.timer.start()
+
 
     def doDamage(self, damage):
         self.hp -= damage
+
+    def dropPowerUp(self):
+        rand = random.random()
+        if rand < self.dropChance:
+            return True
+        else:
+            return False
+
+    def shoots(self):
+        rand = random.random()
+        if rand <= self.perTickShootChance and self.timer.getTimeLeft() <= 0:
+            self.timer.start()
+            return True
+        else:
+            return False
 
 
 class Player(Sprite):
     def __init__(self, scene):
         super().__init__(scene)
-        self.bottomHitboxC = 0.5
-        self.topHitboxC = 0.5
-        self.leftHitboxC = 0.5
-        self.rightHitboxC = 0.5
         self.setImage("INLYSSunny.png")
         self.setSize(60, 60)
-        self.hp = 5
-        self.setHitbox()
+        self.shotTimer = simpleGE.Timer()
+        self.shotTimer.totalTime = 0.2
+        self.shotTimer.start()
 
     def moveSprite(self):
         pos = pygame.mouse.get_pos()
-        self.x = pos[0]
-        self.y = pos[1]
+        if pos == (0, 0):
+            self.heightRatio = self.screenHeight / 1080
+            self.position = (self.screenWidth/2, self.screenHeight - 80*self.heightRatio)
+        else:
+            self.position = pos
 
-    def xGetPos(self):
-        return self.x
-
-    def yGetPos(self):
-        return self.y
-
-    def doDamage(self, damage):
-        self.hp -= damage
-    def process(self):
-        self.moveSprite()
+    def canShoot(self):
+        if self.shotTimer.getTimeLeft() <= 0:
+            self.shotTimer.start()
+            return True
+        else:
+            return False
 
 
 
@@ -174,7 +129,7 @@ class MovingObject(Sprite):
         self.setImage("INLYSSunny.png")
         self.setSize(4, 4)
         self.value = 1
-        self.ySpeed = 3
+        self.moveSpeed = 3
 
     def reset(self):
         # move to where drop comes f
@@ -182,27 +137,15 @@ class MovingObject(Sprite):
         self.y = 50
 
 
-    def checkBounds(self):
-        if self.bottom > self.screenHeight:
-            return True
-        elif self.top < 0:
-            return True
-        elif self.right < 0:
-            return True
-        elif self.left > self.screenWidth:
-            return True
-        else:
-            return False
-
-
 class PowerUp(MovingObject):
     def __init__(self, scene):
         super().__init__(scene)
-        self.setImage("INLYSSunny.png")
-        self.setSize(25, 25)
+        self.setImage("sepsisLaser.png")
+        self.setSize(50, 50)
+        self.dropSpeed = 3
 
     def drop(self):
-        pass
+        self.y += self.dropSpeed
 
 
 
@@ -212,14 +155,12 @@ class Bullet(MovingObject):
         self.damage = 1
         self.setImage("INLYSSunny.png")
         self.setSize(25, 25)
-        self.ySpeed = 10
+        self.moveSpeed = 10
 
-
-    def move(self):
-        self.dy = -self.ySpeed
-
-    def stop(self):
-        pass
+    def moveUp(self):
+        self.y -= self.moveSpeed
+    def moveDown(self):
+        self.y += self.moveSpeed
 
 
 
--- joshuaInvadersMain.py
+++ joshuaInvadersMain.py
@@ -3,32 +3,73 @@ This will be used exclusively to run the game.
 
 """
 
-import simpleGE, pygame, json, gameSprites
-#Custom Events
+import simpleGE, pygame, json, gameSprites, random
+
+# Constants for your convenience
+SCREEN_WIDTH = 1080
+SCREEN_HEIGHT = 720
+
+# Custom Events
 PAUSED = pygame.event.custom_type()
+CONTINUE = pygame.event.custom_type()
+SAVE = pygame.event.custom_type()
+BACK = pygame.event.custom_type()
 
 class Game(simpleGE.Scene):
     def __init__(self):
-        super().__init__()
+        super().__init__(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.mixer.music.load("neverlikedyoursmile.mp3")
-        pygame.mixer.music.set_volume(0.0)
+        pygame.mixer.music.set_volume(0.4)
         self.setImage("scenepark.png")
-        self.waveNumber = 1
+        self.paused = False
+        # timers
+        self.timerContinue = simpleGE.Timer()
+        self.timerContinue.totalTime = 3
+        self.invincibilityTimer = simpleGE.Timer()
+        self.timersndbullet = simpleGE.Timer()
+        self.timersndbullet.totalTime = 4
+        self.timersndbullet.start()
+        # screen adaptability
+        self.width = SCREEN_WIDTH
+        self.widthRatio = SCREEN_WIDTH/1080
+        self.height = SCREEN_HEIGHT
+        self.heightRatio = SCREEN_HEIGHT/720
+        # some details
+        self.enemyNum = 0
+        self.waveNum = 0
+        self.lives = 3
+        self.laserSize = 0
+        self.power = 0
+        self.maxPower = 5
 
-        # put sprites here
+        # sprites
         self.player = gameSprites.Player(self)
-        self.laser = gameSprites.Bullet(self)
+        self.player.setSize(60*self.widthRatio, 60*self.heightRatio)
         self.joshua = gameSprites.Joshua(self)
-        self.bullet = gameSprites.Bullet(self)
-        self.bullet.setImage("boolet.png")
-        # Groups here
-        self.mainGroup = pygame.sprite.Group()
-        self.laserGroup = pygame.sprite.Group()
-        self.playerGroup = pygame.sprite.GroupSingle()
-        self.enemyGroup = pygame.sprite.Group()
-        self.bulletGroup = pygame.sprite.Group()
-        # make sounds here
+        self.misc = []
 
+        # groups
+        self.grpPowerUp = pygame.sprite.Group()
+        self.grpLaser = pygame.sprite.Group()
+        self.grpPlayer = pygame.sprite.GroupSingle()
+        self.grpEnemy = pygame.sprite.Group()
+        self.grpBullet = pygame.sprite.Group()
+        self.grpLblWave = pygame.sprite.GroupSingle()
+        self.grpLblMenu = pygame.sprite.Group()
+        self.grpMisc = pygame.sprite.Group()
+
+        # sounds
+
+        self.sndlaser = gameSprites.Sound("minecraftBowSound.mp3")
+        self.sndlaser.volume(0.4)
+        self.sndbullet = gameSprites.Sound("P226_9mm.mp3")
+        self.sndbullet.volume(1.0)
+        self.sndpowerup = gameSprites.Sound("eatingsound.mp3")
+        self.sndpowerup.volume(0.4)
+        self.sndinlys = gameSprites.Sound("inlys.mp3")
+        self.sndinlys.volume(0.4)
+        self.sndhurt = gameSprites.Sound("steveHurt.mp3")
+        self.sndhurt.volume(0.6)
     def __mainLoop(self):
         """ manage all the main events
             automatically called by start
@@ -41,6 +82,7 @@ class Game(simpleGE.Scene):
             self.processEvent(event)
 
         self.update()
+        self.playerControls()
         self.process()
         for group in self.groups:
             group.clear(self.screen, self.background)
@@ -53,11 +95,14 @@ class Game(simpleGE.Scene):
         """ sets up the sprite groups
             begins the main loop
         """
+        pygame.mouse.set_visible(False)
         self.playerSpriteUp = pygame.sprite.OrderedUpdates(self.player)
-        self.enemySpriteUp = pygame.sprite.OrderedUpdates(self.joshua)
-        self.playerGroup.add(self.playerSpriteUp)
-        self.enemyGroup.add(self.enemySpriteUp)
-        self.groups = [self.laserGroup, self.bulletGroup, self.enemyGroup, self.playerGroup]
+        self.grpPlayer.add(self.playerSpriteUp)
+        self.miscSpriteUp = pygame.sprite.OrderedUpdates(self.misc)
+        self.grpMisc.add(self.miscSpriteUp)
+
+        self.groups = [self.grpLaser, self.grpPowerUp, self.grpBullet, self.grpEnemy, self.grpPlayer,
+                       self.grpLblWave]
 
         self.screen.blit(self.background, (0, 0))
         self.clock = pygame.time.Clock()
@@ -68,8 +113,32 @@ class Game(simpleGE.Scene):
 
     def pauseGame(self):
         # temporary just so I remember
-        pygame.event.post(pygame.event.Event(PAUSED))
-        pass
+        self.paused = True
+        while self.paused:
+            for group in self.groups:
+                group.clear(self.screen, self.background)
+                group.update()
+            for group in self.groups:
+                group.draw(self.screen)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.paused = False
+                self.doEvents(event)
+                self.processEvent(event)
+
+            pygame.display.flip()
+            # continue button
+            if self.isKeyPressed(pygame.K_c):
+                pygame.event.post(pygame.event.Event(CONTINUE))
+                #self.continueTimer.start = time.time()
+                self.paused = False
+                # save button
+            elif self.isKeyPressed(pygame.K_s):
+                pygame.event.post(pygame.event.Event(SAVE))
+                # quit button
+            elif self.isKeyPressed(pygame.K_q):
+                pygame.event.post(pygame.event.Event(pygame.QUIT))
+                self.paused = False
 
     def quitGame(self):
         # temporary just so I remember
@@ -81,35 +150,114 @@ class Game(simpleGE.Scene):
         pass
 
     def doEvents(self, event):
-        # probably for game transitions like waves and stuff
         if event.type == PAUSED:
             self.pauseGame()
+
         if event.type == pygame.QUIT:
             self.saveGame()
             self.stop()
 
-    def process(self):
+    def nextWave(self):
+        if self.enemyNum == 0:
+            self.waveNum += 1
+            if self.waveNum == 1:
+                self.enemyNum = 17
+                for i in range(self.enemyNum):
+                    for j in range(3):
+                        self.joshua = gameSprites.Joshua(self)
+                        self.joshua.setSize(round(self.widthRatio*60), round(self.heightRatio*60))
+                        self.joshua.position = (round((1080-80)/self.enemyNum*(i+1)*self.widthRatio),
+                                                round((80+80*j)*self.heightRatio))
+                        self.joshua.add(self.grpEnemy)
+                self.enemyNum = 3*self.enemyNum
+            elif self.waveNum == 2:
+                pass
+            elif self.waveNum == 3:
+                pass
 
-        for event in pygame.event.get():
-            self.doEvents(event)
-        if self.player.mouseDown:
+    def loseLife(self):
+        self.lives -= 1
+        self.player.position = (round(self.width / 2), self.height - 80*self.heightRatio)
+        pygame.mouse.set_pos(self.player.x, self.player.y)
+        for bullet in self.grpBullet:
+            l = round(self.width/2)-round(200*self.widthRatio)
+            u = round(self.width/2)+round(200*self.widthRatio)
+            if l <= bullet.x <= u:
+                bullet.remove(self.grpBullet)
+
+    def process(self):
+        if self.isKeyPressed(pygame.K_p):
+            pygame.event.post(pygame.event.Event(PAUSED))
+        self.nextWave()
+        for enemy in self.grpEnemy:
+            if enemy.hp <= 0:
+                if enemy.dropPowerUp():
+                    self.powerUp = gameSprites.PowerUp(self)
+                    self.powerUp.setImage("sepsisLaser.png")
+                    self.powerUp.setSize(50*self.widthRatio, 50*self.heightRatio)
+                    self.powerUp.x = enemy.x
+                    self.powerUp.y = enemy.y
+                    self.powerUp.add(self.grpPowerUp)
+                self.enemyNum -= 1
+                enemy.remove(self.grpEnemy)
+            if enemy.shoots() and len(self.grpBullet) <= 10:
+                if self.timersndbullet.getTimeLeft() <= 0:
+                    self.timersndbullet.start()
+                    self.sndbullet.play()
+                self.bullet = gameSprites.Bullet(self)
+                self.bullet.setImage("boolet.png")
+                self.bullet.setSize(30*self.widthRatio, 30*self.heightRatio)
+                self.bullet.moveSpeed = 6
+                self.bullet.setAngle(180)
+                self.bullet.position = enemy.position
+                self.bullet.add(self.grpBullet)
+            if enemy.collidesWith(self.player):
+                self.sndinlys.play()
+                self.loseLife()
+        for powerup in self.grpPowerUp:
+            powerup.drop()
+            if powerup.collidesWith(self.player):
+                self.sndpowerup.play()
+                if self.power < self.maxPower:
+                    self.power += 1
+                    self.laserSize += 5
+                powerup.remove(self.grpPowerUp)
+            elif powerup.checkBounds():
+                powerup.remove(self.grpPowerUp)
+        for bullet in self.grpBullet:
+            bullet.moveDown()
+            if bullet.checkBounds():
+                bullet.remove(self.grpBullet)
+            if bullet.collidesWith(self.player):
+                self.sndinlys.play()
+                bullet.remove(self.grpBullet)
+                self.loseLife()
+        if self.player.mouseDown and self.player.canShoot():
+            self.sndlaser.play()
             self.laser = gameSprites.Bullet(self)
             self.laser.setImage("sepsisLaser.png")
-            self.laser.setSize(25, 25)
-            self.laser.x = self.player.xGetPos()
-            self.laser.y = self.player.yGetPos()
-            self.laser.move()
-            self.laser.add(self.laserGroup)
-        for laser in self.laserGroup:
+            self.laser.setSize((25+self.laserSize)*self.widthRatio, (25+self.laserSize)*self.heightRatio)
+            self.laser.damage += self.power
+            self.laser.x = self.player.x
+            self.laser.y = self.player.y
+            self.laser.add(self.grpLaser)
+
+        for laser in self.grpLaser:
+            laser.moveUp()
             if laser.checkBounds():
-                laser.remove(self.laserGroup)
+                laser.remove(self.grpLaser)
             else:
-                for enemy in self.enemyGroup:
+                for enemy in self.grpEnemy:
                     if laser.collidesWith(enemy):
-                        laser.remove(self.laserGroup)
+                        self.sndhurt.play()
+                        laser.remove(self.grpLaser)
                         enemy.doDamage(laser.damage)
-                    if enemy.hp <= 0:
-                        enemy.remove(self.enemyGroup)
+
+
+    def playerControls(self):
+        if self.isKeyPressed(pygame.K_p):
+            pygame.event.post(pygame.event.Event(PAUSED))
+        self.player.moveSprite()
 
     def update(self):
         isPlaying = pygame.mixer.music.get_busy()
